bceffb0babcddd1470da0cf2d31dfd630cc8068b
sun <1980987589@qq.com>
2025-01-22 20:21:56 -0800

    3

--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -2,7 +2,6 @@ package student;
 
 /**
  * This class is a place holder which you will fully implement based on the javadoc
- *
  * https://cs5004-khoury-lionelle.github.io/hello_world/student/package-summary.html
  *
  */
